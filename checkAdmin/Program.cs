4b7455e01101c1c578b93ea941dfefc8d617a9d9
Necmi Gunduz <necmigunduz@gmail.com>
2024-10-15 11:13:46 +0300

    add checkAdmin challenge solution

---
+++ checkAdmin/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+string? entry;
+bool validString = false;
+
+do
+{
+    entry = Console.ReadLine();
+    validString = entry is not null && entry is "Administrator" or "User" or "Manager";
+    Console.WriteLine(validString
+        ? $"Your input value {entry} has been accepted."
+        : $"The role name that you entered, {entry} is not valid. Enter your role name (Administrator, Manager, or User)");
+
+} while (validString == false);
